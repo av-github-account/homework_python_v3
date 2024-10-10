658d58e47c120974e826b29fdfee2a969cbf2235
av <av.avramenko@gmail.com>
2024-10-10 23:12:35 +0300

    Initial commit

---
+++ ДЗ_2_к_теме_4/ДЗ_2_к_теме_4.py
@@ -0,0 +1,11 @@
+# import math
+import numpy as np
+
+# def my_def_1(arg: float) -> float:
+#     return (math.sqrt(arg))
+#     # t = math.sqrt(arg)
+#     # return (t)
+
+# x = float(input("Enter a number: "))
+# print (my_def_1(x))
+ 
